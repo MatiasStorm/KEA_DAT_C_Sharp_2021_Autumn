2598622db54d4cf37996015bd06a5471f6d80709
MatiasStorm <mat-435@hotmail.com>
2021-09-01 11:07:06 +0200

    Add print pyramid assignment

---
+++ printAPyramid/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace printAPyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("The character used in the triangle: ");
+            char character = Convert.ToChar(Console.ReadLine());
+
+            Console.Write("The height of the triangle: ");
+            int height = Convert.ToInt32(Console.ReadLine());
+
+            int row = 1;
+            string line;
+
+            for(int i = 0; i < height; i++ ){
+                line = "";
+                line += new String(' ', height-i);
+                line += new String(character, row + i);
+                line += new String(' ', height-i);
+
+                Console.WriteLine(line);
+                row++;
+            }
+        }
+    }
+}
